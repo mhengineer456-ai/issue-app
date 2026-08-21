211ec3b629579d4bd42b0c6c9090da88fe9f63d7
Paras Goyal <gparas287@gmail.com>
2026-08-21 12:39:04 +0530

    Add missing babel.config.js for Expo Metro transpilation

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,3 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
-const config = getDefaultConfig(__dirname);
-
-// Exclude non-app scratch test scripts from Metro bundling
-config.resolver.blockList = [
-  /scratch\/.*/,
-];
-
-// Delete deprecated watcher option if present
-if (config.watcher) {
-  delete config.watcher.unstable_workerThreads;
-}
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
